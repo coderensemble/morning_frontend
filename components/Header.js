e6119440f122554316cc1833f0bd13a5d571502c
Adrien NEYRON <adrien.neyron@gmail.com>
2024-02-04 15:55:56 +0100

    New name

--- components/Header.js
+++ components/Header.js
@@ -152,7 +152,7 @@ function Header() {
     <header className={styles.header}>
       <div className={styles.logoContainer}>
         <Moment className={styles.date} date={date} format="MMM Do YYYY" />
-        <h1 className={styles.title}>Daily Sport News</h1>
+        <h1 className={styles.title}>Morning News</h1>
         {userSection}
       </div>
 
